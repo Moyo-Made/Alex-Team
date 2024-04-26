a3b1835e0b4828d50e38898f826502ee9a8a3019
Alameenpd <167607669+Alameenpd@users.noreply.github.com>
2024-04-26 19:26:52 +0000

    adding models to the db

--- proximacare/src/app/action.js
+++ proximacare/src/app/action.js
@@ -1,3 +1,17 @@
-export const DeleteUser = () => {
+import { prisma } from "@/lib/prisma"
+import axios from 'axios'
 
+export default async  function deleteUser() {
+    const user = prisma.user.delete({
+        data : {
+            user,
+        },
+        select : {
+            user : true,
+        }
+    })
+}
+
+export default async function handler (req, res){
+    
 }
