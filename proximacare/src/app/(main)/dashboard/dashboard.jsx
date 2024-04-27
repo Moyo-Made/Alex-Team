c6a28f3511aa896ee8136aa04b130e222a4aed3a
Moyomade <adegbitemoyomade2004@gmail.com>
2024-04-27 08:12:35 +0100

    feat:Added the dashboard

--- proximacare/src/app/(main)/dashboard/dashboard.jsx
+++ proximacare/src/app/(main)/dashboard/dashboard.jsx
@@ -9,8 +9,6 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardHeader,
-  CardTitle,
 } from "@/components/ui/card";
 // import { Input } from "@/components/ui/input";
 import XP from "../../../../public/xp.svg";
