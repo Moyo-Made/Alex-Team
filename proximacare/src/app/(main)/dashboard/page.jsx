b3291632d7e2742bb9a866b834f8c9c033988d27
Moyomade <adegbitemoyomade2004@gmail.com>
2024-04-27 09:12:17 +0100

    Testing push

--- proximacare/src/app/(main)/dashboard/page.jsx
+++ proximacare/src/app/(main)/dashboard/page.jsx
@@ -1,12 +1,12 @@
-import Dashboard from "./dashboard"
+import Dashboard from "./dashboard";
 
-
+const a = 5; // Testing push
 const page = () => {
   return (
     <div>
       <Dashboard />
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
